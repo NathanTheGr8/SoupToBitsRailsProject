0226f2eb15659a1909109c9e9126ae103def1115
Nathan Davis <NathanTheGreat94@gmail.com>
2014-12-19 06:05:50 -0600

    WelcomePage

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,16 @@
 Rails.application.routes.draw do
   resources :categories, :soups
 
-  root to: "categories#index"
+  #root to: "categories#index"
 
-  get "/allcategories", to: redirect("/")
+  get "/allcategories", to: redirect("/categories#index")
 
   get "/soups/:id/toggle_featured", to: "soups#toggle_featured", as: :toggle_featured
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
